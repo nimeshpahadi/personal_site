4c206b770b73c9dc8356bfd1a00a0d888074e384
nimesh <nimesh4477@gmail.com>
2021-03-05 02:12:08 +1100

    Manage media files

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -132,4 +132,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
