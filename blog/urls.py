68f0d5ed3ded3cb5e0bcc874cfb7a989dcaba332
nimesh <nimesh4477@gmail.com>
2021-05-14 00:02:54 +1000

    Change UTC to local time zone

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .feeds import LatestPostsFeed
 
 urlpatterns = [
     path('', views.post_list, name='home'),
-    path('<slug:slug>,<int:id>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/post/<int:id>/', views.post_detail, name='post_detail'),
     path("feed/rss", LatestPostsFeed(), name="post_feed"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
